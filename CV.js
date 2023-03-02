3f53800e90ef3b02939e8b62fb85f0a75c79e652
JesicaQuero <126210374+JesicaQuero@users.noreply.github.com>
2023-03-01 23:02:56 -0300

    Initial commit

---
+++ CV.js
@@ -0,0 +1,19 @@
+
+/** No pude hacer los eventos desde js. Salta error en la función: funtion (){ 
+ 
+    
+document.getElementById('boton').addEventListener('click', function () {
+    console.log("creamos");
+    document.getElementById("prueba").innerHTML = "hhhhhhhhhhhhhh";
+});
+
+document.getElementById('boton-color').addEventListener('click', function () {
+    document.body.style.backgroundColor = '#2EAB07';
+})
+ 
+
+document.getElementById('boton').onclick=function(){
+    console.log("a ver")
+    document.getElementById("prueba").innerHTML = "Hello JavaScript!"
+}
+*/
